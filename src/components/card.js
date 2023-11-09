fed1556dc25e2944c57f014fad31792e51d92a46
Alena Stepanova <kiriyuuhiyori@gmail.com>
2023-11-09 15:41:45 +0300

    like function realised

---
+++ src/components/card.js
@@ -0,0 +1,35 @@
+export { createCard, deleteCard, toggleCardLike };
+
+
+const cardTemplate = document.querySelector("#card-template").content;
+
+function createCard(card, deleteCard, toggleCardLike) {
+  const newCard = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = newCard.querySelector(".card__image");
+  const cardTitle = newCard.querySelector(".card__title");
+  const cardDeleteButton = newCard.querySelector(".card__delete-button");
+  const cardLikeButton = newCard.querySelector(".card__like-button");
+
+  cardImage.src = card.link;
+  cardImage.alt = card.alt;
+  cardTitle.textContent = card.name;
+
+  cardDeleteButton.addEventListener("click", (evt) => {
+    deleteCard(evt);
+  });
+
+  cardLikeButton.addEventListener("click", (evt) => {
+    toggleCardLike(evt);
+  });
+
+  return newCard;
+}
+
+function deleteCard(evt) {
+  const container = evt.target.closest(".card");
+  container.remove();
+}
+
+function toggleCardLike(evt) {
+  evt.target.classList.toggle('card__like-button_is-active');
+}
--- src/index.js
+++ src/index.js
@@ -1,41 +1,15 @@
-import './pages/index.css'; //импорт главного файла стилей
+import './pages/index.css';
+import {initialCards} from './scripts/cards.js';
+import {createCard, deleteCard, toggleCardLike} from './components/card.js';
+
+const cardsContainer = document.querySelector(".places__list");
 
 
-//Шаблон карточки и контейнер
-const cardsContainer = document.querySelector('.places__list');
-const cardTemplate = document.querySelector('#card-template').content;
-
-//Создание карточки
-function createCard(card, deleteCard) {
- const newCard = cardTemplate.querySelector('.card').cloneNode(true);
- const cardImage = newCard.querySelector('.card__image');
- const cardTitle = newCard.querySelector('.card__title');
- const cardDeleteButton =  newCard.querySelector('.card__delete-button');
-
-  cardImage.src = card.link;
-  cardImage.alt = card.alt;
-  cardTitle.textContent = card.name;
-  cardDeleteButton.addEventListener('click', (evt) => {
-      
-    deleteCard(evt);
-    });
-
-  return newCard;
-}
-
-//Добавление карточки
 function addCard(item, itemList) {
-  const cardItem = createCard(item, deleteCard);
+  const cardItem = createCard(item, deleteCard, toggleCardLike);
   itemList.append(cardItem);
 }
 
-//Удаление карточки
-function deleteCard(evt) {
-  const container = evt.target.closest('.card');
-  container.remove();
-}
-
-//Вывод карточек на страницу (вариант с сокр пунктуацией)
 initialCards.forEach( card  => addCard(card, cardsContainer));
 
 
